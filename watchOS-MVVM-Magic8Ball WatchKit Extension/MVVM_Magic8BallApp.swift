daa984e6dfafbd248d0f865b9740e7b74192cef7
Bradley Kang <bkang@lcs.on.ca>
2021-09-27 15:00:31 -0400

    Got a second tab added for watchOS

--- watchOS-MVVM-Magic8Ball WatchKit Extension/MVVM_Magic8BallApp.swift
+++ watchOS-MVVM-Magic8Ball WatchKit Extension/MVVM_Magic8BallApp.swift
@@ -11,8 +11,22 @@ import SwiftUI
 struct MVVM_Magic8BallApp: App {
     @SceneBuilder var body: some Scene {
         WindowGroup {
-            NavigationView {
-                ContentView()
+            
+            TabView {
+                NavigationView {
+                    ContentView()
+                }
+                .tabItem {
+                    Image(systemName: "questionmark.circle")
+                    Text("Ask")
+                }
+                NavigationView {
+                    HistoryView()
+                }
+                .tabItem {
+                    Image(systemName: "clock.fill")
+                    Text("History")
+                }
             }
         }
 
